878113a0cae5497cb368842dc0c20bc326154b0f
haritha12312 <harithapenubadi@gmail.com>
2026-04-18 17:44:28 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,3 +1,7 @@
+
+
+
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
@@ -5,16 +9,18 @@ import time
 def test_add_employee():
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
+    options.add_argument("--no-sandbox")              # IMPORTANT
+    options.add_argument("--disable-dev-shm-usage")   # IMPORTANT
 
     driver = webdriver.Chrome(options=options)
 
-    driver.get("http://20.219.133.124:5000")
+    driver.get("http://20.253.141.56:5000")
 
-    driver.find_element(By.NAME, "name").send_keys("Raju")
+    driver.find_element(By.NAME, "name").send_keys("haritha")
     driver.find_element(By.TAG_NAME, "button").click()
 
     time.sleep(2)
 
-    assert "Raju" in driver.page_source
+    assert "haritha" in driver.page_source
 
     driver.quit()
